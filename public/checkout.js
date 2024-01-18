425bf44ee1d2bbc8eb9e07d8032281a96da220da
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-01-17 22:58:04 -0500

    integrated stripe checkout, need sk and products

---
+++ public/checkout.js
@@ -0,0 +1,19 @@
+const stripe = Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
+initialize();
+
+// Create a Checkout Session as soon as the page loads
+async function initialize() {
+  const response = await fetch("/create-checkout-session", {
+    method: "POST",
+  });
+
+  const { clientSecret } = await response.json();
+
+  const checkout = await stripe.initEmbeddedCheckout({
+    clientSecret,
+  });
+
+  // Mount Checkout
+  checkout.mount('#checkout');
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 require('dotenv').config({path: './config/.env'});
 
@@ -14,6 +15,45 @@ app.use(express.json());
 app.get('/', (req,res)=>{ 
     res.render('index')
 })
+
+//Stripe implementation
+app.post('/create-checkout-session', async (req, res) => {
+    //product creation
+    const product = await stripe.products.create({
+        name: 'T-shirt',
+    });
+
+    //price creation
+    const price = await stripe.prices.create({
+        product: product,
+        unit_amount: 2000,
+        currency: 'usd',
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: 'embedded',
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: price,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      return_url: `http://localhost:${process.env.PORT}/return.html?session_id={CHECKOUT_SESSION_ID}`,
+    });
+  
+    res.send({clientSecret: session.client_secret});
+  });
+
+  app.get('/session-status', async (req, res) => {
+    const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  
+    res.send({
+      status: session.status,
+      customer_email: session.customer_details.email
+    });
+  });
 
 //Server Running
 app.listen(process.env.PORT, () => {
