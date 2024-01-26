daded1b9ce53c6ce319ef12cf142b6b85c32e195
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-01-25 19:53:39 -0500

    added mobile layout

--- public/main.js
+++ public/main.js
@@ -1,3 +1,15 @@
+// NAV
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
 // MODAL
 
 // Get the modal
