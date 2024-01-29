d338ff8ebe6cc0e3dbafa8df25b8efe26d4f28b4
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-01-29 18:20:21 -0500

    finalized mobile layout

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+
 // NAV
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
@@ -14,28 +16,43 @@ function myFunction() {
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var signupModal = document.getElementById("signupModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var signupBtn = document.getElementById("signupBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var signupSpan = document.getElementsByClassName("signupClose")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
   btn.style.zIndex = "0";
 }
+signupBtn.onclick = function() {
+  signupModal.style.display = "block";
+  signupBtn.style.zIndex = "0";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+signupSpan.onclick = function() {
+  signupModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == signupModal) {
+    signupModal.style.display = "none";
   }
 }
 
