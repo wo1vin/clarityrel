ce6ac7e22d8804d6fe5c50cf73b49234b3b1c3cb
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-01-30 15:04:01 -0500

    continuing horizontal layout

--- public/main.js
+++ public/main.js
@@ -15,44 +15,59 @@ function myFunction() {
 // MODAL
 
 // Get the modal
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 var signupModal = document.getElementById("signupModal");
+var registerModal = document.getElementById("registerModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("myBtn");
 var signupBtn = document.getElementById("signupBtn");
+var registerBtn = document.getElementById("registerBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 var signupSpan = document.getElementsByClassName("signupClose")[0];
+var registerSpan = document.getElementsByClassName("registerClose")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  btn.style.zIndex = "0";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+//   btn.style.zIndex = "0";
+// }
 signupBtn.onclick = function() {
   signupModal.style.display = "block";
   signupBtn.style.zIndex = "0";
 }
+registerBtn.onclick = function() {
+  registerModal.style.display = "block";
+  registerBtn.style.zIndex = "0";
+}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 signupSpan.onclick = function() {
   signupModal.style.display = "none";
 }
+registerSpan.onclick = function() {
+  registerModal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 window.onclick = function(event) {
   if (event.target == signupModal) {
     signupModal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == registerModal) {
+    registerModal.style.display = "none";
   }
 }
 
