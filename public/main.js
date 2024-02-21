2c406a70d35da50d202409b919a149b9a0cf4d9f
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-02-21 13:36:52 -0500

    added contact form and edited modal JS

--- public/main.js
+++ public/main.js
@@ -1,42 +1,48 @@
 // NAV
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("navLinks");
-  let nav = document.getElementById("mobileNav");
-  let ham = document.querySelector(".icon");
+const mobileNav = document.getElementById("mobileNav");
+
+function useNav() {
+  const x = document.getElementById("navLinks");
+  const ham = document.querySelector(".icon");
 
   if (x.style.display === "contents") {
     x.style.display = "none";
     ham.style.color = 'gray';
-    nav.style.backgroundColor = '';
+    mobileNav.style.backgroundColor = '';
   } else {
-    nav.style.width = 'max-content';
+    mobileNav.style.width = 'max-content';
     x.style.display = "contents";
     ham.style.color = 'var(--lilac)';
-    nav.style.backgroundColor = '#0a2a2ea3';
+    mobileNav.style.backgroundColor = '#0a2a2ea3';
   }
 }
 
 // MODAL
 // Get the modal
-// Get the button that opens the modal
 const signupModal = document.getElementById("signupModal");
+const contactModal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
 const signupBtn = document.getElementById("signupBtn");
+const contactBtn = document.getElementById("contactBtn");
 
 // Get the <span> element that closes the modal
-var signupSpan = document.getElementsByClassName("signupClose")[0];
+const signupSpan = document.getElementsByClassName("signupClose")[0];
+const contactSpan = document.getElementsByClassName("contactClose")[0];
 
 // When the user clicks on the button, open the modal
-signupBtn.addEventListener('click', () => {
-  signupModal.style.display = "block";
-  signupBtn.style.zIndex = "0";
-})
+signupBtn.addEventListener('click', signupToggle);
+contactBtn.addEventListener('click', contactToggle);
 
-// registerBtn.onclick = function() {
-//   registerModal.style.display = "block";
-//   registerBtn.style.zIndex = "0";
-// }
-
+function signupToggle (){
+    signupModal.style.display = "block";
+    signupBtn.style.zIndex = "0";
+}
+function contactToggle (){
+    contactModal.style.display = "block";
+    contactBtn.style.zIndex = "0";
+}
 // When the user clicks on <span> (x), close the modal
 // span.onclick = function() {
 //   modal.style.display = "none";
@@ -44,28 +50,27 @@ signupBtn.addEventListener('click', () => {
 signupSpan.onclick = function() {
   signupModal.style.display = "none";
 }
-// registerSpan.onclick = function() {
-//   registerModal.style.display = "none";
-// }
+contactSpan.onclick = function() {
+  contactModal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == signupModal) {
     signupModal.style.display = "none";
   } 
-  if (event.target == registerModal) {
-    registerModal.style.display = "none";
+  if (event.target == contactModal) {
+    contactModal.style.display = "none";
   }
-  if (event.target == signupModalD) {
-    signupModalD.style.display = "none";
-  }
-  if (event.target == registerModalD) {
-    registerModalD.style.display = "none";
+  if (event.target == mobileNav) {
+    mobileNav.style.display = "none";
   }
 }
 
-var registerBtn = document.getElementById("registerBtn");
+// get the "Register" button
+const registerBtn = document.getElementById("registerBtn");
 registerBtn.addEventListener('click',register);
+// send users to the /register.html
 function register(){
   console.log('get register');
   window.location.href = '/register';
